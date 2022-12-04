5cb1fdeb8c90dfe277ecf644f1b233bb01fe5b62
Adam Foster <adamfoster@gmail.com>
2022-12-04 16:47:53 +1100

    Day 4

---
+++ day04/day04part2.py
@@ -0,0 +1,23 @@
+#answer = 12740
+
+#filename = 'sample01.txt'
+filename = 'input.txt'
+
+score = 0
+with open(filename, "r") as f:
+    for line in f:
+        [s1,s2] = line.strip().split(',')
+        #print(line.strip().split(','))
+        #print(s1)
+        [s1a, s1b] = map(int, s1.split("-"))
+        [s2a, s2b] = map(int, s2.split("-"))
+        if s1a<=s2a and s2a<=s1b:
+            score += 1
+        elif s1a<=s2b and s2b<=s1b:
+            score += 1
+        elif s2a<=s1a and s1a<=s2b:
+            score += 1
+        elif s2a<=s1b and s1b<=s2b:
+            score += 1
+print(score)
+
