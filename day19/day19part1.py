bd50fe8760a742db0a3585f0e9a16e2bbf448417
Adam Foster <adamfoster@gmail.com>
2022-12-22 12:00:42 +1100

    Day 19 part 1. Not fast, but working

--- day19/day19part1.py
+++ day19/day19part1.py
@@ -1,10 +1,10 @@
-#answer = 
+#answer = 1480
 
 from dataclasses import dataclass, field
 from copy import deepcopy
 
-filename = 'sample01.txt'
-#filename = 'input.txt'
+#filename = 'sample01.txt'
+filename = 'input.txt'
 
 TYPES = ["ore", "clay", "obsidian", "geode"]
 TYPES_R = ["geode", "obsidian", "clay", "ore"]
@@ -18,6 +18,7 @@ class Robot:
 class Blueprint:
     index: int
     robots: dict[str, Robot]
+    maxes: dict[str, bool]
 
 @dataclass
 class State:
@@ -51,20 +52,26 @@ class State:
 blueprints = []
 with open(filename, "r") as f:
     for i,line in enumerate(f):
-        b = Blueprint(index=i, robots={})
+        b = Blueprint(index=i, robots={}, maxes={t:0 for t in TYPES})
         for robotstr in line.strip().split(":")[1].strip()[:-1].split("."):
             #print("*",robotstr,"*")
             roboparts = robotstr.strip().split(" ")
             t = roboparts[1].strip()
             costs = {}
             costs[roboparts[5].strip()] = int(roboparts[4].strip())
+            
             if len(roboparts) > 6:
                 costs[roboparts[8].strip()] = int(roboparts[7].strip())
             b.robots[t] = Robot(minetype=t, costs=costs)
+        #maxes = {t:max([b.robots[r].costs[t] for r in b.robots if t in b.robots[r].costs]) for t in TYPES}
+        for rid,r in b.robots.items():
+            for cid,c in r.costs.items():
+                b.maxes[cid] = max(b.maxes[cid], c)
+        
         blueprints.append(b)
 
 print(blueprints)
-#exit(0)
+
 MAXTIME = 24
 
 def prunekey(resources):
@@ -79,12 +86,12 @@ def geodes_collected(blueprint: Blueprint, state: State, cache: dict, prune: dic
     #if dict != None:
     #    assert False
 
-    print("STATE", state, len(cache))
+    #print("STATE", state, len(cache))
     if state.timeleft == 0:
         #exit(0)
         return 0
     if state in cache:
-        print("HIT")
+        #print("HIT")
         return cache[state]
     if prunekey(state.resources) in prune:
         previous = prune[prunekey(state.resources)]
@@ -96,18 +103,18 @@ def geodes_collected(blueprint: Blueprint, state: State, cache: dict, prune: dic
                 if previous[1][t] <= state.robots[t]:
                     prevbetter = False
         if prevbetter:
-            print("PRUNE")
+            #print("PRUNE")
             return 0
     maxsubgeodes = 0
 
-    for t in TYPES[:-1]: # reject if too many resources
-        if state.resources[t] > 2*(sum([r.costs[t] for _,r in blueprint.robots.items() if t in r.costs])):
-            print("PRUNE 3")
-            return 0
+    #for t in TYPES[:-1]: # reject if too many resources
+    #    if state.resources[t] > 2*(sum([r.costs[t] for _,r in blueprint.robots.items() if t in r.costs])):
+    #        #print("PRUNE 3")
+    #        return 0
 
     couldmakeall = True
     for t in TYPES_R: #make robot
-        if state.canafford(blueprint.robots[t]):
+        if state.canafford(blueprint.robots[t]) and (t == "geode" or blueprint.maxes[t] > state.robots[t]):
             s = deepcopy(state)
             for t2 in TYPES:
                 s.resources[t2] += s.robots[t2]
@@ -131,9 +138,10 @@ def geodes_collected(blueprint: Blueprint, state: State, cache: dict, prune: dic
         for t in TYPES:
             s.resources[t] += s.robots[t]
         maxsubgeodes = max(maxsubgeodes, geodes_collected(blueprint, s, cache, prune))
-    else: print("PRUNE 2")
+    #else: print("PRUNE 2")
     geodes = maxsubgeodes + state.robots["geode"]
     cache[state] = geodes
+    #print(cache[state])
     prune[prunekey(state.resources)] = (state.timeleft, state.robots)
     return geodes #amount mined in subticks + this tick
 
@@ -143,7 +151,11 @@ def geodes_collected_starter(blueprint: Blueprint):
     resources = {t: 0 for t in TYPES}
     robots["ore"] = 1
     state = State(robots=robots, resources=resources, timeleft=MAXTIME)
-    geodes_collected(blueprint=blueprint, state=state, cache=cache, prune={})
+    return geodes_collected(blueprint=blueprint, state=state, cache=cache, prune={})
 
-print (geodes_collected_starter(blueprint=blueprints[0]))
+quality = 0
+for i,bp in enumerate(blueprints):
+    v = geodes_collected_starter(blueprint=bp)
+    quality += (i+1)*v
+    print (i,v,quality)
 
